7df666a367e376bbf6dfe3d9ae75a8f830d259ed
Matthieu Falce <falce.matthieu@gmail.com>
2016-05-14 15:22:52 +0200

    rename

--- src/main/Main.java
+++ src/main/Main.java
@@ -22,38 +22,44 @@ import captors.LineObserver;
 import captors.Movement;
 import captors.WallDetectors;
 import captors.WallObserver;
+import java.io.*;
+import lejos.nxt.*;
+import lejos.nxt.comm.*;
 
-public class Main {
+/**
+ * sample of selecting channel at run time
+ */
+public class Main
+{
 
 	public static void main(String[] args) {
-		RConsole.openUSB(0);
 		System.setOut(new PrintStream(RConsole.getPrintStream()));
 		System.out.println("caca prout");
-		
-		
+
+
 		BluetoothRobot br = new BluetoothRobot();
 		Thread btThread = new Thread(br);
 		btThread.start();
-		
+
 		LineDetectors ld = new LineDetectors ();
 		Thread ldThread = new Thread (ld);
 		ldThread.start ();
-		
+
 		WallDetectors wd = new WallDetectors ();
 		Thread wdThread = new Thread(wd);
 		wdThread.start();
-		
+
 		Position position = new Position(0.5, 0.5, Direction.EAST);
-		
+
 		System.out.println("Go");
-		
+
 		Movement move = new Movement(position);
 		LineObserver lo = new LineObserver(move, position);
 		ld.addObserver(lo);
-		
+
 		WallObserver wo = new WallObserver();
 		wd.addObserver(wo);
-		
+
 		// actions
 //		Grid g = GridGenerator.generate(23, 11, 0.2);
 		//Grid g = new Grid(3, 4);
@@ -61,7 +67,7 @@ public class Main {
 //		g.addWall(1, 1, Direction.EAST);
 //		g.addWall(2, 3, Direction.NORTH);/**/
 		Grid g = GridExample.g;
-		
+
 		IA ia = new IA(position, g);
 		Point dest = new Point(9, 22);
 		Solution sol = ia.goTo(dest.x, dest.y);
@@ -83,7 +89,7 @@ public class Main {
 		System.out.println("avant gc : " + Runtime.getRuntime().freeMemory());
 		System.gc();
 		System.out.println("apres gc : " + Runtime.getRuntime().freeMemory());
-		
+
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
@@ -92,7 +98,7 @@ public class Main {
 		System.out.println("cost : " + cost);
 //		move.followPath(path);
 		//System.out.println(Runtime.getRuntime().freeMemory());
-		
+
 		/*Map<Point, Integer> tests = new Map<Point, Integer>();
 		tests.put(position.getPoint(), 0);
 		System.out.println(ia.parkoor(tests, position.getPoint(), new Point(22, 10)).size());
@@ -102,14 +108,14 @@ public class Main {
 		System.gc();
 		System.out.println(ia.allPossibleDestinations(tests, position.getPoint()).size());
 		System.out.println(Runtime.getRuntime().freeMemory());/**/
-		
+
 //		Motor.C.rotateTo(0);
 		ld.stop();
 		wd.stop();
-//		
+//
 		System.out.println("Main ended");
 //		System.out.println(Motor.A.isMoving());
 		Button.waitForAnyPress(20000);
 	}
-	
+
 }
