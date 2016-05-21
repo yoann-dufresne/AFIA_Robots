d0002cffbc6005b5cf3665e0f9c5a6c5122f9375
Erwsaym <erwsaym@hotmail.fr>
2016-05-21 16:47:59 +0200

    IA explorer pour NXT

---
+++ src/ia/AbstractExplorer.java
@@ -0,0 +1,104 @@
+package ia;
+
+import graphicalInterface.Window;
+
+import java.awt.Point;
+
+import mock.Movement;
+import mock.WallDetectors;
+import mock.WallObserver;
+import model.Direction;
+import model.Grid;
+import model.Position;
+
+/**
+ *	Class controlling the exploration phase
+ */
+public abstract class AbstractExplorer{
+
+	protected WallDetectors wd;
+	protected WallObserver wo;
+
+	protected Position position;
+	protected Movement move;
+
+	protected Grid grid;
+	protected Window window;
+
+	protected int XMax;
+	protected int YMax;
+
+
+	public AbstractExplorer(Position position, Movement move, WallDetectors wd, WallObserver wo, Grid grid, Window w){
+		this.position = position;
+		this.move = move;
+
+		this.grid = grid;
+
+		this.wd = wd;
+		this.wo = wo;
+
+		this.window = w;
+
+		XMax = grid.getHeight();
+		YMax = grid.getWidth();
+	}
+
+	public abstract void explore();
+
+
+	protected int explore_tile(boolean useA, int min_wallDistance) {
+		int nb_case_detected_right, nb_case_detected_left;
+		nb_case_detected_right = this.wo.getNbCase_A();
+		nb_case_detected_left = this.wo.getNbCase_B();
+		addDetectedWall(
+				this.wo.getOrientation(false),
+				nb_case_detected_right);
+
+		addDetectedWall(
+				this.wo.getOrientation(true), 
+				nb_case_detected_left);
+
+		if(useA){ // A = right
+			min_wallDistance = Math.min(min_wallDistance, nb_case_detected_right);
+
+		}else { // B = left
+			min_wallDistance = Math.min(min_wallDistance, nb_case_detected_left);
+		}
+		return min_wallDistance;
+	}
+
+	protected void exploreTurningHead(){
+		//		this.explore_tile(false, 100);
+		//		this.wd.changeHeadPosition();
+		//		this.explore_tile(true, 100);
+		Direction dir = this.wo.getOrientation(false);
+		addDetectedWall(
+				this.wo.getOrientation(false),
+				this.wo.getNbCase_A());
+		dir = this.wo.getOrientation(true);
+		addDetectedWall(
+				this.wo.getOrientation(true),
+				this.wo.getNbCase_B());
+		this.wd.changeHeadPosition();
+		addDetectedWall(
+				this.wo.getOrientation(false),
+				this.wo.getNbCase_A());
+		addDetectedWall(
+				this.wo.getOrientation(true),
+				this.wo.getNbCase_B());
+	}
+
+	protected void addDetectedWall(Direction direction, int nb_case){
+		Point point = this.position.getPoint();
+		for(int i=Math.min(6,nb_case); i>0; --i){
+			this.grid.setDiscovered(point.x, point.y, direction);
+			this.grid.translatePoint(point, direction);
+		}
+		if(nb_case < 6)
+			this.grid.addWall(point.x, point.y, direction);
+
+		window.refreshWithPause(100);
+	}
+}
+
---
+++ src/ia/WallValuesExplorer.java
@@ -0,0 +1,175 @@
+package ia;
+
+import graphicalInterface.Window;
+
+import java.awt.Point;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import mock.Movement;
+import mock.WallDetectors;
+import mock.WallObserver;
+import model.Direction;
+import model.Grid;
+import model.Position;
+import model.Tile;
+import model.WallState;
+
+public class WallValuesExplorer extends AbstractExplorer {
+
+	protected int tileValues[][];
+	
+	public WallValuesExplorer(Position position, Movement move, WallDetectors wd, WallObserver wo, Grid grid, Window w) {		
+		super(position, move, wd, wo, grid, w);
+		this.tileValues = new int[XMax][YMax];
+	}
+
+	@Override
+	public void explore () {
+	//Tant que tout n'est pas découvert
+		//Recherche de la plus grande valeur dans tileValues
+		//Bouger jusqu'à la case associée à la valeur
+		//  --> Si on rencontre un mur, on stoppe et on update les valeurs
+		this.exploreTurningHead();
+		this.computeScores(this.position.getPoint());
+		this.printTiles();
+		this.goToBest();
+		this.computeScores(this.position.getPoint());
+		this.printTiles();
+	}
+	
+	public void goToBest(){
+		Point highPoint = this.findHighestScore();
+		this.position.turnRight();
+		for(int i=0; i < highPoint.x; i++){
+			this.move.forward(1);
+			this.exploreTurningHead();
+		}
+		this.position.turnLeft();
+		for(int i=0; i < highPoint.y; i++){
+			this.move.forward(1);
+			this.exploreTurningHead();
+		}
+	}
+	
+	public void goToBestTest(){
+		this.position.turnRight();
+		for(int i=0; i < 6; i++){
+			this.move.forward(1);
+			this.exploreTurningHead();
+			this.computeScores(this.position.getPoint());
+			this.printTiles();
+		}
+		
+		this.position.turnLeft();
+		for(int i=0; i < 12; i++){
+			this.move.forward(1);
+			this.exploreTurningHead();
+			this.computeScores(this.position.getPoint());
+			this.printTiles();
+		}
+
+		this.position.turnLeft();
+		for(int i=0; i < 6; i++){
+			this.move.forward(1);
+			this.exploreTurningHead();
+			this.computeScores(this.position.getPoint());
+			this.printTiles();
+		}
+
+	}
+	
+	public Point findHighestScore(){
+		Point highPoint = new Point();
+		int highScore;
+		highScore=0;
+		for (int y=0; y<YMax; y++){
+			for (int x=0; x<XMax; x++){
+				if (this.tileValues[x][y] > highScore){
+					highPoint= new Point(x,y);
+					highScore= this.tileValues[x][y];
+				}
+			}
+		}
+		return highPoint;
+	}
+	
+	public void computeScores(Point p){
+		for(int y = 0; y < YMax; y++){
+			for(int x = 0; x < XMax; x++){
+				int score = this.tileScore(x, y);
+				if (score != 0)
+					this.tileValues[x][y] = (int) Math.max(1,score - p.distance(x,y));
+				else 
+					this.tileValues[x][y]= 0;
+//				this.tileValues[x][y]= score;
+			}
+		}
+	}
+
+	
+	public void printTiles(){
+		for(int x = 0; x < XMax; x++){
+			for(int y = 0; y < YMax; y++){
+				if (x == this.position.getPoint().x && y == this.position.getPoint().y)
+					System.out.print(" x ");
+				else
+					System.out.print(String.format("%03d", tileValues[x][y]));
+				System.out.print(" ");
+			}
+			System.out.println(" ");
+		}
+		System.out.println("\n");
+	}
+	
+	/** for a given point get the number of undiscovered wall in all directions
+	@return a mark corresponding to the max number of wall that can be discovered from this tile
+	*/
+	public int tileScore(int x, int y) {
+		int mark = 0;
+		Point point = new Point(x, y);
+		for (Direction d : Direction.values()) {
+			mark += getValuesByDirection(point, d); 
+		}
+		return mark;
+
+	}
+	
+	
+	/**
+	get the number of tiles without wall on the given direction from the given point
+	*/
+	public int getValuesByDirection(Point originPoint, Direction d) {
+		int mark = 0;
+		
+		WallState state;
+		Tile tile;
+		
+		tile = this.grid.getTile(originPoint);
+		Point point = new Point(originPoint);
+		if (tile == null)
+			return mark;
+		
+		state = tile.getState(d);
+		
+		int offset = 0;
+		while (offset < 6 && state != WallState.Wall){
+			this.grid.translatePoint(point,d);
+			
+			if(state == WallState.Undiscovered){
+				mark++; 
+			}
+			
+			offset++;
+			tile = this.grid.getTile(point);
+			if (tile == null)
+				return mark;
+			
+			state = tile.getState(d);
+		}
+		
+		return mark;
+	}
+}
