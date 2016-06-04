e634c8dc20b1eb44070448bf8f69e6c326fa92bd
Erwsaym <erwsaym@hotmail.fr>
2016-06-04 16:28:28 +0200

    Enlever les import vu deprecated par lejos

--- src/ia/WallValuesExplorer.java
+++ src/ia/WallValuesExplorer.java
@@ -2,9 +2,9 @@ package ia;
 
 import java.awt.Point;
 //import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+//import java.util.HashSet;
+//import java.util.List;
+//import java.util.Set;
 
 import captors.Movement;
 import captors.WallDetectors;
