602114f2a705ca076e23b0e3ea68d6de29119854
Matthieu Falce <falce.matthieu@gmail.com>
2016-06-19 01:02:29 +0200

    parsing messages

--- bluetoothPC/code/app.js
+++ bluetoothPC/code/app.js
@@ -5,8 +5,12 @@ client.connect(9999, '127.0.0.1', function() {
   console.log('Connected');
 });
 
-client.on('data', function(data) {
-  console.log('Received: ' + data);
+client.on('data', function(datas) {
+  console.log('Received: ' + datas);
+  datas = JSON.parse(datas);
+  for(var robot in datas)
+    var message = datas[robot];
+    processData(robot, message);
 });
 
 client.on('close', function() {
@@ -15,17 +19,24 @@ client.on('close', function() {
 
 
 var getInfos = function(){
-  console.log("query log");
   client.write("log");
-
 }
-
 setInterval(getInfos, 1000)
-
 
 var quit = function(){
   client.write("stop");
   process.exit();
+}
+
+var processData = function(robot, messages){
+  for(var i=0; i < messages.length; i++){
+    var message = messages[i];
+    var arr = message.split(";");
+    var command = arr[0];
+    arr.shift();
+    var arguments = arr;
+    console.log(command, arguments);
+  }
 }
 
 process.on('SIGINT', quit);
